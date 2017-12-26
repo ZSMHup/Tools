379f2143478fe1c5af0227000da0e9da4d344b65
张书孟 <zhang_shu_meng@163.com>
2017-12-26 13:04:12 +0800

    【commit】：update

--- Tools/3rdLib/FileDownloadManager/FileDownloadManager.h
+++ Tools/3rdLib/FileDownloadManager/FileDownloadManager.h
@@ -11,79 +11,85 @@
 #import "FileSessionModel.h"
 
 @interface FileDownloadManager : NSObject
+
 /**
- *  单例
- *
- *  @return 返回单例对象
+ 单例
+
+ @return @return 返回单例对象
  */
 + (instancetype)sharedInstance;
 
 /**
  开启任务下载资源
- 
+
  @param url 下载地址
+ @param fileName 存储的目录 默认：Default
  @param attribute 需要存储的属性
  @param progressBlock 回调下载进度
  @param stateBlock 下载状态
  */
-//- (void)download:(NSString *)url attribute:(NSDictionary *)attribute progress:(void(^)(NSInteger receivedSize, NSInteger expectedSize, CGFloat progress))progressBlock state:(void(^)(DownloadState state))stateBlock;
 - (void)downloadWithUrl:(NSString *)url fileName:(NSString *)fileName attribute:(NSDictionary *)attribute progress:(void(^)(NSInteger receivedSize, NSInteger expectedSize, CGFloat progress))progressBlock state:(void(^)(DownloadState state))stateBlock;
 
 /**
- *  开始下载
+ 开始下载
+
+ @param url 下载地址
  */
 - (void)start:(NSString *)url;
 
-
 /**
- *  暂停下载
+ 暂停下载
+ 
+ @param url 下载地址
  */
 - (void)pause:(NSString *)url;
 
 /**
- *  查询该资源的下载进度值
- *
- *  @param url 下载地址
- *
- *  @return 返回下载进度值
+ 查询该资源的下载进度值
+
+ @param url 下载地址
+ @param fileName 文件存放的目录 默认：default
+ @return 返回下载进度值
  */
-//- (CGFloat)progress:(NSString *)url;
 - (CGFloat)progress:(NSString *)url fileName:(NSString *)fileName;
 
 /**
- *  获取该资源总大小
- *
- *  @param url 下载地址
- *
- *  @return 资源总大小
+ 获取该资源总大小
+
+ @param url 下载地址
+ @param fileName 该资源的存放目录 默认：default
+ @return 资源总大小
  */
-//- (NSInteger)fileTotalLength:(NSString *)url;
 - (NSInteger)fileTotalLength:(NSString *)url fileName:(NSString *)fileName;
 
 /**
- *  判断该资源是否下载完成
- *
- *  @param url 下载地址
- *
- *  @return YES: 完成
+ 判断该资源是否下载完成
+
+ @param url 下载地址
+ @param fileName 该资源的存放目录 默认：default
+ @return YES: 完成
  */
 - (BOOL)isCompletion:(NSString *)url fileName:(NSString *)fileName;
 
 /**
- *  判断该文件是否存在
+ 判断该文件是否存在
+
+ @param url 下载地址
+ @param fileName 该资源的存放目录 默认：default
+ @return YES: 存在
  */
 - (BOOL)isExistence:(NSString *)url fileName:(NSString *)fileName;
 
 /**
- *  删除该资源
- *
- *  @param url 下载地址
- *  @param fileName 存放的文件夹
+ 删除该资源
+
+ @param url 下载地址
+ @param fileName 存放的文件夹
  */
 - (void)deleteFile:(NSString *)url fileName:(NSString *)fileName;
 
 /**
- *  清空所有下载资源
+ 清空所有下载资源
  */
 - (void)deleteAllFile;
 
--- Tools/3rdLib/FileDownloadManager/FileSessionModel.h
+++ Tools/3rdLib/FileDownloadManager/FileSessionModel.h
@@ -20,20 +20,14 @@ typedef enum {
 
 /** 流 */
 @property (nonatomic, strong) NSOutputStream *stream;
-
 /** 下载地址 */
 @property (nonatomic, copy) NSString *url;
-
 /** 获得服务器这次请求 返回数据的总长度 */
 @property (nonatomic, assign) NSInteger totalLength;
-
-//自己增加的属性 -----------------------------------
+/** 可以存储一些文件属性 */
 @property (nonatomic, copy) NSDictionary *attribute;
-// ----------------------------------------------
-
 /** 下载进度 */
 @property (nonatomic, copy) void(^progressBlock)(NSInteger receivedSize, NSInteger expectedSize, CGFloat progress);
-
 /** 下载状态 */
 @property (nonatomic, copy) void(^stateBlock)(DownloadState state);
 
